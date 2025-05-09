b9bbfa6d1cb9ba4a7d1057484e87091ce6899949
Ming0099 <130200978+Ming0099@users.noreply.github.com>
2025-05-10 00:08:59 +0900

    Create 09 B13909.java

---
+++ 15.약수, 배수와 소수 2/09 B13909.java
@@ -0,0 +1,8 @@
+import java.util.*;
+public class B13909 {
+    public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in);
+        int N = scanner.nextInt();
+        System.out.println((int)Math.sqrt(N));
+    }
+}
