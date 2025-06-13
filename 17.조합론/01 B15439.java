e447626ed77619356f6cc32d398a7782800fbe56
Ming0099 <130200978+Ming0099@users.noreply.github.com>
2025-06-13 23:46:49 +0900

    Create 01 B15439.java

---
+++ 17.조합론/01 B15439.java
@@ -0,0 +1,8 @@
+import java.util.*;;
+public class B15439 {
+    public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in);
+        int N = scanner.nextInt();
+        System.out.println(N * (N - 1));
+    }
+}
