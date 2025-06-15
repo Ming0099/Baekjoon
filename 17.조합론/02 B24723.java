06a2544304d82b139411703e3b841c1a7ab48cca
Ming0099 <130200978+Ming0099@users.noreply.github.com>
2025-06-15 18:54:20 +0900

    Create 02 B24723.java

---
+++ 17.조합론/02 B24723.java
@@ -0,0 +1,8 @@
+import java.util.*;
+public class B24723 {
+    public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in);
+        int N = scanner.nextInt();
+        System.out.println((int)Math.pow(2, N));
+    }
+}
